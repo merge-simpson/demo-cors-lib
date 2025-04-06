0450baf5e39c7207d3ecb9330da8641607cf025a
merge-simpson <letsdev@kakao.com>
2025-04-06 21:45:40 +0900

    feat(cors): add cors properties, config. (activated draft)

---
+++ src/main/java/nettee/cors/properties/CorsProperties.java
@@ -0,0 +1,19 @@
+package nettee.cors.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+
+@ConfigurationProperties("app.cors")
+@ConfigurationPropertiesBinding
+public record CorsProperties(
+        MappedCorsProperties[] endpoints
+) {
+    // compact constructor
+    // record는 반드시 AllArgsConstructor에서 인스턴스 생성
+    //  그 직전에 거치는 곳이 compact 생성자
+    public CorsProperties {
+        for (var item : endpoints) {
+            System.out.println(item);
+        }
+    }
+}
